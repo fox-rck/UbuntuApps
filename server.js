47ce1b84d3e3f2ffb05e273c603fd14659577fce
fox-rck <fox-rck@gmail.com>
2014-11-22 21:15:16 -0600

    'build'

--- server.js
+++ server.js
@@ -28,7 +28,15 @@ var formidable = require('formidable'),
         console.log('Time:', Date.now());
         next();
     });
-
+    router.get('/', function (req, res, next) {
+        res.writeHead(200, { 'content-type': 'text/html' });
+        res.end(
+            '<form action="/upload" enctype="multipart/form-data" method="post">' +
+            '<input type="file" name="file" multiple="multiple"><br>' +
+            '<input type="submit" value="Upload">' +
+            '</form>'
+        );
+    });
     // a middleware sub-stack shows request info for any type of HTTP request to /user/:id
     router.use('/file/:id', function (req, res, next) {
         console.log('Request URL:', req.originalUrl);
@@ -74,6 +82,40 @@ var formidable = require('formidable'),
         res.end('special:' + req.params.id);
     });
 
+    // handler for /user/:id which renders a special page
+    router.post('/upload', function (req, res, next) {
+        upload(req, res, next)
+    });
+
+    function upload(req, res, next) {
+                // parse a file upload
+                var form = new formidable.IncomingForm();
+
+                form.parse(req, function (err, fields, files) {
+                    // `file` is the name of the <input> field of type `file`
+                    var old_path = files.file.path,
+                        file_size = files.file.size,
+                        file_ext = files.file.name.split('.').pop(),
+                        index = old_path.lastIndexOf('/') + 1,
+                        file_name = old_path.substr(index),
+                        new_path = path.join(process.env.PWD, '/uploads/', file_name + '.' + file_ext);
+
+                    fs.readFile(old_path, function (err, data) {
+                        fs.writeFile(new_path, data, function (err) {
+                            fs.unlink(old_path, function (err) {
+                                if (err) {
+                                    res.writeHead(500);
+                                    res.end('error');
+                                } else {
+                                    res.writeHead(200);
+                                    res.end(new_path);
+                                }
+                            });
+                        });
+                    });
+                });
+                return;
+            }
     // mount the router on the app
     app.use('/', router);
 
