489ec50d15b3b58a9c6b2199d6e51b7956b5a116
fox-rck <fox-rck@gmail.com>
2014-11-22 21:25:35 -0600

    'build'

--- server.js
+++ server.js
@@ -107,9 +107,12 @@ var formidable = require('formidable'),
                                     res.writeHead(500);
                                     res.end('error');
                                 } else {
-                                    var newElm = collection.insert({ name: file_name, ext: file_ext })
-                                    res.writeHead(200);
-                                    res.end(newElm._id);
+                                    var newElm = collection.insert({ name: file_name, ext: file_ext }, function (err, docsInserted) {
+                                        console.log(docsInserted);
+                                        res.writeHead(200);
+                                        res.end(docsInserted._id);
+                                    })
+                                 
                                 }
                             });
                         });
